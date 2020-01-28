3ec462cdad9b9222d0a34ec736d1d2396ed016b7
Cameron May <camerondmay@gmail.com>
2020-01-29 07:39:10 +1300

    Update dotscape.js

--- dotscape.js
+++ dotscape.js
@@ -299,7 +299,7 @@ function touchEnded() {
 }
 
 function touchdown(ev) {
-  	ev.preventDefault();
+
   if (introState === 3) {
     for (let i = 0; i < dotsCount; i++) {
       dots[i].getCol(winMouseX, winMouseY);
@@ -320,6 +320,7 @@ function touchdown(ev) {
 }
 
 function touchMoved() {
+  	ev.preventDefault();
   if (introState === 3) {
     for (let i = 0; i < dotsCount; i++) {
       dots[i].clicked(winMouseX, winMouseY);
